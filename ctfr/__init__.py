79731a5f3c19f9ebb4d4a9d0b5b74df65e77fdb0
rsscml <rahul.sinhaocp@gmail.com>
2022-05-11 15:26:15 +0530

    CTFR

---
+++ ctfr/__init__.py
@@ -0,0 +1,3 @@
+from ctfr.ctfr_data import ctfr_dataset
+from ctfr.ctfr_losses import RMSE, Huber, QuantileLoss_v2, Normal_NLL_Loss, Poisson_NLL_Loss, Negbin_NLL_Loss, supported_losses
+from ctfr.ctfr_models import Simple_ConvTransformer, Feature_Weighted_ConvTransformer

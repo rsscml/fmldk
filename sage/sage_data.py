bugfix is standard scaling scale incorporation

sage/sage_data.py
@@ -605,7 +605,7 @@ class sage_dataset:
             target_scaled = np.divide(target, target_nz_mean)
         elif self.scaling_method == 'standard_scaling':
             target_mean = np.mean(target[:, :max_input_len, :], axis=1, keepdims=True)
-            target_stddev = np.std(target[:, :max_input_len, :], axis=1, keepdims=True)
+            target_stddev = np.maximum(np.std(target[:, :max_input_len, :], axis=1, keepdims=True), 0.001)
             target_scaled = np.divide(np.subtract(target, target_mean), target_stddev)
             target_scaled = np.nan_to_num(target_scaled)  # correct where stddev is 0
         
@@ -643,7 +643,7 @@ class sage_dataset:
                 known_num = np.divide(known_num, known_nz_mean)
             elif self.scaling_method == 'standard_scaling':
                 known_mean = np.mean(known_num, axis=1, keepdims=True)
-                known_stddev = np.std(known_num, axis=1, keepdims=True)
+                known_stddev = np.maximum(np.std(known_num, axis=1, keepdims=True), 0.001)
                 known_num = np.divide(np.subtract(known_num, known_mean), known_stddev)
                 known_num = np.nan_to_num(known_num)
             # merge
@@ -664,7 +664,7 @@ class sage_dataset:
                 unknown_num = np.divide(unknown_num, unknown_nz_mean)
             elif self.scaling_method == 'standard_scaling':
                 unknown_mean = np.mean(unknown_num[:, :max_input_len, :], axis=1, keepdims=True)
-                unknown_stddev = np.std(unknown_num[:, :max_input_len, :], axis=1, keepdims=True)
+                unknown_stddev = np.maximum(np.std(unknown_num[:, :max_input_len, :], axis=1, keepdims=True), 0.001)
                 unknown_num = np.divide(np.subtract(unknown_num, unknown_mean), unknown_stddev)
                 unknown_num = np.nan_to_num(unknown_num)
             # merge

sage/sage_model.py
@@ -933,7 +933,7 @@ class SageTransformer_Model(tf.keras.Model):
         # Create Numerical Embedding (Linear Transform) Layers
         
         self.target_linear_transform_layer = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(units=d_model, use_bias=False)) 
-        #self.scale_linear_transform_layer = tf.keras.layers.Dense(units=d_model, use_bias=False)
+        self.scale_linear_transform_layer = tf.keras.layers.Dense(units=d_model, use_bias=False)
         self.hist_encode_layer = CausalConvEncoder(d_model=d_model, num_layers=self.num_causal_layers) 
       
         if len(self.stat_num_col_names)>0:
@@ -1031,10 +1031,10 @@ class SageTransformer_Model(tf.keras.Model):
             decoder_vars_list.append(rel_age_dec)
             # scale
             scale = tf.strings.to_number(inputs[:, :-1, -3:-1], out_type=tf.dtypes.float32)
-            #scale_log = scale[:,:,0:1] #tf.math.log(tf.math.sqrt(tf.abs(scale[:,:,0:1]))) # mean
-            #scale_log = self.scale_linear_transform_layer(scale_log[:,-1,:])
+            scale_log = scale[:,:,0:1] #tf.math.log(tf.math.sqrt(tf.abs(scale[:,:,0:1]))) # mean
+            scale_log = self.scale_linear_transform_layer(scale_log[:,-1,:])
             # append
-            #static_vars_list.append(scale_log)
+            static_vars_list.append(scale_log)
         elif r_dim == 1:
             # print(" mean r_dim")
             # rel_age
@@ -1044,13 +1044,13 @@ class SageTransformer_Model(tf.keras.Model):
             decoder_vars_list.append(rel_age_dec)
             # scale
             scale = tf.strings.to_number(inputs[:, :-1, -2:-1], out_type=tf.dtypes.float32)
-            #scale_log = scale #tf.math.log(tf.math.sqrt(scale))
-            #scale_log = self.scale_linear_transform_layer(scale_log[:,-1,:])
+            scale_log = scale #tf.math.log(tf.math.sqrt(scale))
+            scale_log = self.scale_linear_transform_layer(scale_log[:,-1,:])
             # append
-            #static_vars_list.append(scale_log)
+            static_vars_list.append(scale_log)
         
         # Append additional columns
-        #stat_cols_ordered_list = stat_cols_ordered_list + ['scale']
+        stat_cols_ordered_list = stat_cols_ordered_list + ['scale']
         future_cols_ordered_list = future_cols_ordered_list + ['rel_age']
         
         # mask
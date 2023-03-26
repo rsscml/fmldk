b78d6af7865799c11b6d4aa2e992e50d8a218a95
rsscml <rahul.sinhaocp@gmail.com>
2023-03-26 09:30:16 +0530

    global scaling added to sage,tft,ctfrv2

--- ctfrv2/__init__.py
+++ ctfrv2/__init__.py
@@ -1,3 +1,4 @@
 from ctfrv2.ctfrv2_data import ctfrv2_dataset
+from ctfrv2.ctfrv2_global_scaled_data import ctfrv2_gs_dataset
 from ctfrv2.ctfrv2_losses import RMSE, Huber, QuantileLoss_v2, Normal_NLL_Loss, Poisson_NLL_Loss, Negbin_NLL_Loss, supported_losses
 from ctfrv2.ctfrv2_models import Feature_Weighted_ConvTransformer
--- tft/__init__.py
+++ tft/__init__.py
@@ -1,4 +1,5 @@
 
 from tft.tft_data import tft_dataset
+from tft.tft_global_scaled_data import tft_gs_dataset
 from tft.tft_losses import RMSE, Huber, QuantileLoss_v2, Normal_NLL_Loss, Poisson_NLL_Loss, Negbin_NLL_Loss, supported_losses
 from tft.tft_model import Temporal_Fusion_Transformer
